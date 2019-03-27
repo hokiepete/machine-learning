0a13f9b70e277facdfc96d57d24ad061d9a646e6
Peter <pnolan1986@gmail.com>
2019-03-27 00:23:49 -0400

    Update chapter-7.py

--- chapter-7.py
+++ chapter-7.py
@@ -34,3 +34,75 @@ bag_clf.fit(x_train,y_train)
 y_pred = bag_clf.predict(x_val)
 print(bag_clf.__class__.__name__,accuracy_score(y_val,y_pred))
 
+from sklearn.ensemble import RandomForestClassifier
+rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=1)
+rnd_clf.fit(x_train,y_train)
+y_pred = rnd_clf.predict(x_val)
+#print(y_pred)
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=1)
+rnd_clf.fit(iris["data"],iris["target"])
+for name, score in zip(iris["feature_names"],rnd_clf.feature_importances_):
+    print(name, score)
+
+from sklearn.ensemble import AdaBoostClassifier
+ada_clf = AdaBoostClassifier(
+        DecisionTreeClassifier(max_depth=1), n_estimators=200,
+        algorithm="SAMME.R", learning_rate=0.5
+        )
+ada_clf.fit(x_train,y_train)
+y_pred = ada_clf.predict(x_val)
+#print(y_pred)
+
+from sklearn.tree import DecisionTreeRegressor
+tree_reg1 = DecisionTreeRegressor(max_depth=2)
+tree_reg1.fit(x,y)
+
+y2 = y - tree_reg1.predict(x)
+tree_reg2 = DecisionTreeRegressor(max_depth=2)
+tree_reg2.fit(x,y2)
+
+y3 = y2 - tree_reg2.predict(x)
+tree_reg3 = DecisionTreeRegressor(max_depth=2)
+tree_reg3.fit(x,y3)
+
+y_pred = sum(tree.predict(x) for tree in (tree_reg1,tree_reg2,tree_reg3))
+
+from sklearn.ensemble import GradientBoostingRegressor
+gbrt = GradientBoostingRegressor(max_depth=2,n_estimators=3, learning_rate=1.0)
+gbrt.fit(x,y)
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+xtrain, xval, ytrain, yval = train_test_split(x,y)
+
+gbrt = GradientBoostingRegressor(max_depth=2,n_estimators=120)
+gbrt.fit(xtrain,ytrain)
+
+errors =[mean_squared_error(yval,ypred) for ypred in gbrt.staged_predict(xval)]
+bst_n_estimators = np.argmin(errors)
+
+gbrt_best = GradientBoostingRegressor(max_depth=2,n_estimators=bst_n_estimators)
+gbrt_best.fit(xtrain,ytrain)
+
+gbrt = GradientBoostingRegressor(max_depth=2,warm_start=True)
+min_val_error = float("inf")
+error_going_up = 0
+for n_estimators in range(1,320):
+    gbrt.n_estimators = n_estimators
+    gbrt.fit(xtrain,ytrain)
+    ypred = gbrt.predict(xval)
+    val_error = mean_squared_error(yval,ypred)
+    if val_error < min_val_error:
+        min_val_error=val_error
+        error_going_up=0
+    else:
+        error_going_up+=1
+        if error_going_up==5:
+            break
+
+
